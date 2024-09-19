08a9bdb6df84f273da7efb9d293a3eb24eb3ab6e
rkuklik <rkuklik@expect-it.cz>
2024-09-19 16:21:22 +0200

    nixos(system): basic system info and config

---
+++ modules/nixos/system.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  system,
+  ...
+}: let
+  inherit
+    (lib)
+    listToAttrs
+    mkOption
+    ;
+
+  options = ["distroName" "distroId" "codeName"];
+  value = mkOption {readOnly = false;};
+  writeable = name: {inherit name value;};
+in {
+  options.system.nixos = listToAttrs (map writeable options);
+  config = {
+    system = {
+      stateVersion = "24.11";
+      nixos = {
+        distroName = "kytkOS";
+        distroId = "nixos";
+        codeName = "Seed";
+      };
+    };
+    nixpkgs = {
+      hostPlatform = system;
+      config = {
+        allowUnfree = true;
+        use-xdg-base-directories = true;
+      };
+    };
+    nix = {
+      settings = {
+        experimental-features = ["nix-command" "flakes"];
+        auto-optimise-store = true;
+      };
+    };
+  };
+}
