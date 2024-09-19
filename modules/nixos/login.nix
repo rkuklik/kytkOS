becf61a6552de6f6767cbde10b791e5aa4328a03
rkuklik <rkuklik@expect-it.cz>
2024-09-19 21:00:57 +0200

    nixos(login): introduce login framework

---
+++ modules/nixos/login.nix
@@ -0,0 +1,23 @@
+{
+  options,
+  config,
+  lib,
+  ...
+}: let
+  opts = options.kytkos.login;
+  cfg = config.kytkos.login;
+  providers = lib.attrNames opts;
+  enabled = name: cfg.${name}.enabled or false;
+  toInt = bool:
+    if bool
+    then 1
+    else 0;
+  count = builtins.foldl' (acc: bool: acc + toInt bool) 0 (map enabled providers);
+in {
+  config.assertions = [
+    {
+      assertion = count != 0 -> count == 1;
+      message = "At most one login manager can be enabled";
+    }
+  ];
+}
