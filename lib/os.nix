4567653240cc6ea90c6cbb54a4ea0f1fffd3885d
rkuklik <rkuklik@expect-it.cz>
2024-09-19 15:55:28 +0200

    lib(os): add system set

---
+++ lib/os.nix
@@ -0,0 +1,16 @@
+lib: let
+  systems = {
+    linux = {
+      x86_64 = "x86_64-linux";
+      aarch64 = "aarch64-linux";
+    };
+    darwin = {
+      x86_64 = "x86_64-darwin";
+      aarch64 = "aarch64-darwin";
+    };
+  };
+in {
+  inherit
+    systems
+    ;
+}
