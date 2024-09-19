2ecb6ae1987d7ea5b761d640dbe003fea3c1f5bb
rkuklik <rkuklik@expect-it.cz>
2024-09-19 12:57:05 +0200

    lib: source all modules

--- flake.nix
+++ flake.nix
@@ -1,7 +1,21 @@
 {
   description = "kytkOS";
 
-  outputs = {nixpkgs, ...} @ inputs: {
+  outputs = {nixpkgs, ...} @ inputs: let
+    bootstrap = import ./lib/fs.nix nixpkgs.lib;
+    flowerModule = entity: {
+      name = entity.base;
+      value = import entity.path nixpkgs.lib;
+    };
+    flower =
+      builtins.listToAttrs
+      (
+        builtins.map
+        flowerModule
+        (bootstrap.filters.nix (bootstrap.treeList ./lib))
+      );
+  in {
+    inherit flower;
     nixosConfigurations = {};
   };
 
