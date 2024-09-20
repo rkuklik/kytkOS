f592e1b613483bf683bd23cb6e77618610125da3
rkuklik <rkuklik@expect-it.cz>
2024-09-20 10:07:34 +0200

    rkuklik(nvim): enable treesitter

---
+++ users/rkuklik/nvim/treesitter.nix
@@ -0,0 +1,37 @@
+{
+  programs.nixvim.plugins = {
+    treesitter = {
+      enable = true;
+      folding = true;
+      settings = {
+        highlight = {
+          enable = true;
+          additional_vim_regex_highlighting = false;
+        };
+        incremental_selection = {
+          enable = true;
+          keymaps =
+            {
+              init_selection = "<C-space>";
+              node_incremental = "<C-space>";
+              scope_incremental = "<C-s>";
+              node_decremental = "<C-backspace>";
+            }
+            // {
+              init_selection = "gnn";
+              node_incremental = "grm";
+              scope_incremental = "grn";
+              node_decremental = "grc";
+            };
+        };
+        indent.enable = true;
+      };
+    };
+    treesitter-context = {
+      enable = true;
+    };
+    treesitter-textobjects = {
+      enable = true;
+    };
+  };
+}
