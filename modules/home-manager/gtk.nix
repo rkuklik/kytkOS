94d61cc17f51cbfebf53e56a4d81af2529942bba
rkuklik <rkuklik@expect-it.cz>
2024-09-20 09:44:47 +0200

    hm(gtk): move gtk2 config and get rid of nonsense

---
+++ modules/home-manager/gtk.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  os,
+  ...
+}: let
+  gtk2rc = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+  gtk2rcDefault = "${config.home.homeDirectory}/.gtkrc-2.0";
+  backup = name: "${name}.${os.home-manager.backupFileExtension}";
+  delete = [gtk2rcDefault (backup gtk2rcDefault) (backup gtk2rc)];
+  spaced = lib.concatStringsSep " ";
+  files = map (name: ''"${name}"'') delete;
+in {
+  gtk.gtk2.configLocation = gtk2rc;
+  home.file.${gtk2rc} = {
+    text = lib.mkDefault "";
+    force = true;
+    onChange =
+      # bash
+      ''
+        for file in ${spaced files}; do
+          if [[ -e $file ]]; then
+            rm "$backup"
+          fi
+        done
+      '';
+  };
+}
