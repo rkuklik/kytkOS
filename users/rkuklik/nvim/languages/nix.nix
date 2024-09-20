0af9effbf3e0fbeef31343bd271ab2c3091493f9
rkuklik <rkuklik@expect-it.cz>
2024-09-20 10:23:22 +0200

    rkuklik(nvim.lang): add nix

---
+++ users/rkuklik/nvim/languages/nix.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  programs.nixvim.plugins = {
+    lsp.servers = {
+      nil-ls.enable = true;
+    };
+    conform-nvim.settings = {
+      formatters_by_ft.nix = {
+        __unkeyed-alejandra = "alejandra";
+      };
+      formatters = {
+        alejandra.command = lib.getExe pkgs.alejandra;
+        nixfmt.command = lib.getExe pkgs.nixfmt-rfc-style;
+      };
+    };
+  };
+}
