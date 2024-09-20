aab07c093e069c5d7944d471aa9b1b28e3251a22
rkuklik <rkuklik@expect-it.cz>
2024-09-20 10:14:40 +0200

    rkuklik(nvim): add cmp

---
+++ users/rkuklik/nvim/cmp.nix
@@ -0,0 +1,105 @@
+let
+  inherit (builtins) map;
+  named = name: {inherit name;};
+  mapping = body: {__raw = "cmp.mapping.${body}";};
+  select = "{ behavior = cmp.SelectBehavior.Select }";
+  darkarts =
+    # lua
+    ''
+      function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return true
+          and col ~= 0
+          and vim.api
+                  .nvim_buf_get_lines(0, line - 1, line, true)[1]
+                  :sub(col, col)
+                  :match("%s")
+              == nil
+      end
+    '';
+  selector = body:
+  # lua
+  ''
+    cmp.mapping(function(fallback)
+      local luasnip = require("luasnip")
+        ${body}
+      end, { "i", "s" })
+  '';
+in {
+  programs.nixvim.plugins.cmp = {
+    enable = true;
+    settings = {
+      sources = map named [
+        "nvim_lsp"
+        "luasnip"
+        "buffer"
+        "path"
+        "crates"
+      ];
+      completion.completeopt = "menu,menuone,noinsert,noselect";
+      snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+      mapping = {
+        "<C-n>" = mapping "select_next_item(${select})";
+        "<C-p>" = mapping "select_prev_item(${select})";
+        "<C-b>" = mapping "scroll_docs(-4)";
+        "<C-f>" = mapping "scroll_docs(4)";
+        "<C-Space>" = mapping "complete()";
+        "<C-e>" = mapping "abort()";
+        "<CR>" = mapping "confirm({ select = true })";
+        "<S-CR>" =
+          mapping
+          # lua
+          ''
+            confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = true,
+            })
+          '';
+        "<Tab>" =
+          selector
+          # lua
+          ''
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif (${darkarts})() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          '';
+        "<S-Tab>" =
+          selector
+          # lua
+          ''
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          '';
+      };
+    };
+    cmdline = let
+      search = {
+        mapping.__raw = "cmp.mapping.preset.cmdline()";
+        sources = map named ["buffer"];
+        view.entries = {
+          name = "wildmenu";
+          separator = " | ";
+        };
+      };
+      command = {
+        mapping.__raw = "cmp.mapping.preset.cmdline()";
+        sources = map named ["path" "cmdline"];
+      };
+    in {
+      "/" = search;
+      "?" = search;
+      ":" = command;
+    };
+  };
+}
