93a5f4a417ff9061fb0537faa730e32204aa3f33
rkuklik <rkuklik@expect-it.cz>
2024-09-22 10:50:27 +0200

    rkuklik(plasma): add kwin effects

--- users/rkuklik/plasma/kwin.nix
+++ users/rkuklik/plasma/kwin.nix
@@ -1,16 +1,24 @@
 {
-  programs.plasma.kwin.virtualDesktops = {
-    rows = 1;
-    names = [
-      "One"
-      "Two"
-      "Three"
-      "Four"
-      "Five"
-      "Six"
-      "Seven"
-      "Eight"
-      "Nine"
-    ];
+  programs.plasma.kwin = {
+    virtualDesktops = {
+      rows = 1;
+      names = [
+        "One"
+        "Two"
+        "Three"
+        "Four"
+        "Five"
+        "Six"
+        "Seven"
+        "Eight"
+        "Nine"
+      ];
+    };
+    effects = {
+      desktopSwitching.animation = "slide";
+      blur.enable = true;
+      shakeCursor.enable = true;
+      snapHelper.enable = true;
+    };
   };
 }
--- users/rkuklik/plasma/manager.nix
+++ users/rkuklik/plasma/manager.nix
@@ -6,9 +6,9 @@
 }: {
   imports = [inputs.plasma-manager.homeManagerModules.plasma-manager];
   config = lib.mkIf os.kytkos.desktop.plasma.enable {
-    programs = {
-      plasma.enable = true;
-      plasma.overrideConfig = true;
+    programs.plasma = {
+      enable = true;
+      overrideConfig = false;
     };
     stylix.targets.kde.enable = false;
   };
