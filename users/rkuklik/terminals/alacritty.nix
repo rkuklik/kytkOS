cd9192d46a13d1979e98689d111da1398054559f
rkuklik <rkuklik@expect-it.cz>
2024-09-26 13:44:43 +0200

    rkuklik(term): add terminal config

--- users/rkuklik/terminals/alacritty.nix
+++ users/rkuklik/terminals/alacritty.nix
@@ -1,5 +1,19 @@
-{config, ...}: let
-  cfg = config.flowerbed.terminal;
-in {
-  programs.alacritty.enable = cfg.enable;
+{
+  config,
+  lib,
+  ...
+}: {
+  programs.alacritty = {
+    enable = lib.mkDefault config.flowerbed.terminal.enable;
+    settings = {
+      font.builtin_box_drawing = true;
+      window = {
+        dynamic_padding = false;
+        dynamic_title = false;
+        startup_mode = "Windowed";
+        title = "Alacritty";
+        blur = true;
+      };
+    };
+  };
 }
---
+++ users/rkuklik/terminals/wezterm.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  programs.wezterm = {
+    enable = lib.mkDefault config.flowerbed.terminal.enable;
+    extraConfig =
+      # lua
+      ''
+        return {
+          use_fancy_tab_bar = false,
+          warn_about_missing_glyphs = false,
+          enable_kitty_graphics = true,
+          hide_tab_bar_if_only_one_tab = true,
+          window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0,
+          },
+        }
+      '';
+  };
+}
