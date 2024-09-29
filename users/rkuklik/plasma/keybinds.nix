0a2f9a78f37758229315a9bda90b743001fc2ae3
rkuklik <rkuklik@expect-it.cz>
2024-09-29 22:32:47 +0200

    hm(plasma): remove keybinds module

--- users/rkuklik/plasma/keybinds.nix
+++ users/rkuklik/plasma/keybinds.nix
@@ -1,6 +1,86 @@
 {
-  flowerbed.plasma.keybinds.kwin = {
-    fullscreen = "Meta+F";
-    close = "Meta+Q";
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit
+    (lib)
+    listToAttrs
+    getExe
+    mkIf
+    mod
+    range
+    ;
+  inherit (builtins) toString;
+
+  desktopMapper = name: base:
+    listToAttrs
+    (map (num: {
+        name = "${name} ${toString num}";
+        value = "${base}${toString (mod num 10)}";
+      })
+      (range 1 10));
+  screenMapper = name: base:
+    listToAttrs
+    (map (num: {
+        name = "${name} ${toString (num - 1)}";
+        value = "${base}${toString num}";
+      })
+      (range 1 8));
+
+  inherit
+    (config.programs)
+    alacritty
+    wezterm
+    ;
+  alacrittyLauncher = pkgs.writeShellApplication {
+    name = "Alacritty";
+    runtimeInputs = [alacritty.package];
+    text = "alacritty msg create-window || alacritty";
+  };
+in {
+  programs.plasma = {
+    shortcuts.kwin =
+      {
+        "Window Fullscreen" = "Meta+F";
+        "Window Close" = "Meta+Q";
+        "Kill Window" = "Meta+Ctrl+Esc";
+        "MoveMouseToCenter" = "Meta+C";
+        "Window Move Center" = "Meta+Shift+C";
+        "Overview" = ["Meta" "Meta+W"];
+        "Window No Border" = "Meta+X";
+
+        "Window Quick Tile Bottom" = "Meta+Down";
+        "Window Quick Tile Left" = "Meta+Left";
+        "Window Quick Tile Right" = "Meta+Right";
+        "Window Quick Tile Top" = "Meta+Up";
+
+        "Window One Desktop Down" = "Meta+Ctrl+Shift+Down";
+        "Window One Desktop Up" = "Meta+Ctrl+Shift+Up";
+        "Window One Desktop to the Left" = "Meta+Ctrl+Shift+Left";
+        "Window One Desktop to the Right" = "Meta+Ctrl+Shift+Right";
+      }
+      // (desktopMapper "Switch to Desktop" "Meta+")
+      // (desktopMapper "Window to Desktop" "Meta+Ctrl+")
+      // (screenMapper "Switch to Screen" "Meta+Alt+")
+      // (screenMapper "Window to Screen" "Meta+Alt+Ctrl+");
+    hotkeys.commands = {
+      alacritty = mkIf alacritty.enable {
+        command = getExe alacrittyLauncher;
+        key = "Meta+Shift+Return";
+        comment = "Launch Alacritty";
+      };
+      wezterm = mkIf wezterm.enable {
+        command = getExe wezterm.package;
+        key = "Meta+Return";
+        comment = "Launch WezTerm";
+      };
+      kcalc = {
+        command = getExe pkgs.kdePackages.kcalc;
+        key = "Calculator";
+        comment = "Launch KCalc";
+      };
+    };
   };
 }
