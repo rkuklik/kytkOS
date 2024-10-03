5b4e86e6b393ce04ab0797f44184c3e571ed0cba
rkuklik <rkuklik@expect-it.cz>
2024-10-03 16:28:58 +0200

    rkuklik(email): add accounts

---
+++ users/rkuklik/accounts/email.nix
@@ -0,0 +1,52 @@
+{config, ...}: {
+  accounts.email = {
+    maildirBasePath = "${config.xdg.dataHome}/maildir";
+    accounts = {
+      centrum = {
+        address = "kuklik.radek@centrum.cz";
+        userName = "kuklik.radek";
+        realName = "Kuklík Radek";
+        primary = true;
+        thunderbird.enable = true;
+        imap = {
+          host = "imap.centrum.cz";
+          port = 993;
+          tls = {
+            enable = true;
+            useStartTls = false;
+          };
+        };
+        smtp = {
+          host = "smtp.centrum.cz";
+          port = 587;
+          tls = {
+            enable = true;
+            useStartTls = true;
+          };
+        };
+      };
+      expect-it = {
+        address = "rkuklik@expect-it.cz";
+        userName = "rkuklik";
+        realName = "Kuklík Radek";
+        thunderbird.enable = true;
+        imap = {
+          host = "mail.expect-it.cz";
+          port = 9993;
+          tls = {
+            enable = true;
+            useStartTls = false;
+          };
+        };
+        smtp = {
+          host = "mail.centrum.cz";
+          port = 9025;
+          tls = {
+            enable = true;
+            useStartTls = true;
+          };
+        };
+      };
+    };
+  };
+}
