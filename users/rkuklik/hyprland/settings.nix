c6952f6ea1f2d81c95091a11279a8bced2de2da2
rkuklik <rkuklik@expect-it.cz>
2025-01-24 12:27:45 +0100

    feat(hypr): add basic keybinds

--- users/rkuklik/hyprland/settings.nix
+++ users/rkuklik/hyprland/settings.nix
@@ -6,10 +6,85 @@
 }:
 let
   inherit (lib)
+    flatten
     getExe
+    mod
+    mapAttrsToList
+    range
     ;
 
-  mod = "SUPER";
+  workspaces = range 1 10;
+  numeric = [
+    (id: "${keys.main}, ${toString (mod id 10)}, workspace, ${toString id}")
+    (id: "${keys.main} ${keys.ctrl}, ${toString (mod id 10)}, movetoworkspace, ${toString id}")
+  ];
+  mkFocusBind = _: def: map (key: "${keys.main}, ${key}, movefocus, ${def.hypr}") def.keys;
+  mkSwapBind =
+    _: def: map (key: "${keys.main} ${keys.alt}, ${key}, swapwindow, ${def.hypr}") def.keys;
+  direction = {
+    left = {
+      hypr = "l";
+      keys = [
+        "left"
+        "h"
+      ];
+    };
+    right = {
+      hypr = "r";
+      keys = [
+        "right"
+        "l"
+      ];
+    };
+    up = {
+      hypr = "u";
+      keys = [
+        "up"
+        "k"
+      ];
+    };
+    down = {
+      hypr = "d";
+      keys = [
+        "down"
+        "j"
+      ];
+    };
+  };
+
+  keys = {
+    shift = "SHIFT";
+    main = "SUPER";
+    ctrl = "CTRL";
+    alt = "ALT";
+  };
+
+  bind = flatten [
+    [
+      "${keys.main}, Return, exec, ${getExe config.programs.alacritty.package}"
+      "${keys.main}, q, killactive"
+      "${keys.main}, f, fullscreen, 1"
+      "${keys.main} ${keys.shift}, f, fullscreen, 0"
+      "${keys.main} ${keys.shift}, c, centerwindow"
+    ]
+    (map (key: "${keys.main} ${keys.ctrl}, ${key}, workspace, -1") direction.left.keys)
+    (map (key: "${keys.main} ${keys.ctrl}, ${key}, workspace, +1") direction.right.keys)
+    (map (
+      key: "${keys.main} ${keys.ctrl} ${keys.shift}, ${key}, movetoworkspace, -1"
+    ) direction.left.keys)
+    (map (
+      key: "${keys.main} ${keys.ctrl} ${keys.shift}, ${key}, movetoworkspace, +1"
+    ) direction.right.keys)
+    (map (f: map f workspaces) numeric)
+    (mapAttrsToList mkFocusBind direction)
+    (mapAttrsToList mkSwapBind direction)
+  ];
+  bindm = [
+    "${keys.main}, mouse:272, movewindow"
+    "${keys.main}, mouse:273, resizewindow"
+    "${keys.main} ${keys.alt}, mouse:272, resizewindow"
+  ];
+  workspace = map (id: "${toString id}, persistent:true") workspaces;
 in
 {
   wayland.windowManager.hyprland = {
@@ -17,9 +92,12 @@ in
     settings = {
       monitor = ",preferred,auto,auto";
       general = {
-        gaps_in = 5;
-        gaps_out = 5;
+        gaps_in = 2;
+        gaps_out = 4;
         border_size = 2;
+      };
+      gestures = {
+        workspace_swipe = true;
       };
       decoration = {
         shadow = {
@@ -28,17 +106,15 @@ in
       };
       input = {
         numlock_by_default = true;
-        natural_scroll = true;
+        touchpad = {
+          natural_scroll = true;
+        };
       };
-      bind = [
-        "${mod}, Return, exec, ${getExe config.programs.alacritty.package}"
-        "${mod}, q, killactive"
-      ];
-      bindm = [
-        "${mod}, mouse:272, movewindow"
-        "${mod}, mouse:273, resizewindow"
-        "${mod} ALT, mouse:272, resizewindow"
-      ];
+      inherit
+        bind
+        bindm
+        workspace
+        ;
     };
   };
 }
